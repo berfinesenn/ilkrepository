f64470b029d73cd006672dc689610d2c3dd8a7b3
berfinesenn <esenbef@gmail.com>
2024-07-10 18:06:43 +0300

    tekrardan değişkik yaptım bağlarken

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Archi's Academy Github Kursuna Hoşgeldiniz!
+          Git % GitHub öğreniyorum w/berfinesenn yenisi
         </p>
         <a
           className="App-link"
