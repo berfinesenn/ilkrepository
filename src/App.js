09f614ccc05259f6bfab82d52f499a2243f7a089
berfinesenn <esenbef@gmail.com>
2024-07-10 23:30:40 +0300

    yeni comit yeni vieo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Git % GitHub öğreniyorum w/berfinesenn yenisi
+          HOŞGELDİNİZ
+        </p>
+        <p>
+          Yeni eklenti 2
+        </p>
+        <p>
+          Yeni eklenti 3
+        </p>
+        <p>
+          Git & GitHub öğreniyorum w/berfinesen
         </p>
         <a
           className="App-link"
